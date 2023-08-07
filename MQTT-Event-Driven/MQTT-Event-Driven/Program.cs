ca2428372039bafae47f9d75875c0a2f4ab097d6
Maylis Grune <grunem.tin22@student.dhbw-heidenheim.de>
2023-08-07 00:46:43 -0700

    Added Configutaion manager

---
+++ MQTT-Event-Driven/MQTT-Event-Driven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlTypes;
+
+
+namespace MQTT_Event_Driven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+        }
+
+        static void Init()
+        {
+            ConfigManager.BuildConfig();
+        }
+    }
+}
