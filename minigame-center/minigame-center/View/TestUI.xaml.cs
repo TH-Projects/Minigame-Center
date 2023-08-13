aa984a29b6fa9a075b944854c42647c5a4524c4b
vv-ab <vivianberger028@gmail.com>
2023-08-13 16:21:22 +0200

    460-Logik der UI Elemente

--- minigame-center/minigame-center/View/TestUI.xaml.cs
+++ minigame-center/minigame-center/View/TestUI.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,10 +23,11 @@ namespace minigame_center.View
     /// </summary>
     public partial class TestUI : Page
     {
+
         public TestUI()
         {
             InitializeComponent();
-            DataContext = new TestUIViewModel();
         }
     }
 }
+
