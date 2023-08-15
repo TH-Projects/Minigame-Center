e0aafff8e2fedfd9a25f101494e70f914b7ee23b
Marcel Hasselberg <hasselberg.marcel@gmail.com>
2023-08-15 14:11:49 +0200

    Aufsetzen der Spiellogik

---
+++ Game_Logic/Game_Logic/Connect_Four.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Logic
+{
+
+    public enum GameResult
+    {
+        Won,
+        Draw,
+        Running
+    }
+
+    internal class Connect_Four
+    {
+        public string[,] GameField { get; set; }
+        private string clinetID;
+        private int Field_X;
+        private int Field_Y;
+
+
+        public Connect_Four(int Field_X ,  int Field_Y, string clientID)
+        {
+            this.clinetID = clientID;
+            GameField = new string[Field_Y, Field_X];
+            
+            for (int i = 0; i < Field_Y; i++)
+            {
+                for (int j = 0; j < Field_X; j++)
+                {
+                    GameField[i, j] = "empty";
+                }   
+            }
+        }
+
+
+        public bool SetStonePossible(int Current_X)
+        {
+            for (int i = 0; i < Field_Y; i++){ 
+                if (GameField[i, Current_X] == "empty")
+                {
+                    return true;
+                }
+            }
+                return false;
+        }
+
+        public GameResult SetStone(int Current_X)
+        {
+            int Current_Y = 0;
+            for(int i = Field_Y -1; i >0; i--)
+            {
+                if (GameField[i, Current_X] == "empty")
+                {
+                    GameField[i, Current_X] = clinetID;
+                    Current_Y = i;
+                }
+            }
+            return CheckIfPlayerWon(Current_X, Current_Y);
+        }
+
+
+        private GameResult CheckIfPlayerWon(int Current_X, int Current_Y)
+        {
+            GameResult gameResult;
+            bool EmptyFieldExist = false;
+            for (int i = 0; i < Field_Y; i++)
+            {
+                for (int j = 0; j < Field_X; j++)
+                {
+                    if (GameField[i, j] == "empty")
+                    {
+                        EmptyFieldExist = true;
+                    }
+                }
+            }
+            int Count = 0;
+
+
+            for (int i = 0; i < Field_X; i++)
+            {
+              if (GameField[Current_Y, i] == clinetID)
+                {
+                    Count++;
+                    if (Count == 4)
+                    {
+                        gameResult = GameResult.Won;
+                    }
+                }else
+                {
+                    Count = 0;
+                }
+            }
+
+
+            for (int i = 0; i < Field_Y; i++)
+            {
+                if (GameField[i, Current_X] == clinetID)
+                {
+                    Count++;
+                    if (Count == 4)
+                    {
+                        gameResult = GameResult.Won;
+                    }
+                }
+                else
+                {
+                    Count = 0;
+                }
+            }
+
+         
+            int Diagnonal_X = Current_X;
+            int Diagnonal_Y = Current_Y; 
+
+
+
+           while(Diagnonal_X > 0 && Diagnonal_Y > 0)
+                Diagnonal_X -= 1;
+                Diagnonal_Y -= 1;
+            }
+
+            for (int i = 0; i < Diagnonal_Y + i && i < Diagnonal_X + i; i++)
+            {
+                if (GameField[Diagnonal_Y + i, Diagnonal_X + i] == clinetID)
+                {
+                    Count++;
+                    if (Count == 4)
+                    {
+                        gameResult = GameResult.Won;
+                    }
+                }
+                else
+                {
+                    Count = 0;
+                }
+            }
+        }
+    }
+}
---
+++ MQTT_PublicBroker_Connection_Class/MQTT_PublicBroker_Connection/Program.cs
@@ -0,0 +1,144 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace MQTT_PublicBroker_Connection
+{
+
+    class MessageData
+    {
+        public string senderId { get; set; }
+        public string messageType { get; set; }
+        public string content { get; set; }
+
+    }
+    
+
+    class MQTTConnection
+    {
+        const string  brokerAddress = "25aee1926b284dfeb459111a517f7201.s2.eu.hivemq.cloud";
+        const int brokerPort = 8883;
+
+        const string username = "minigame_inf22_dhbw";
+        const string password = "Or4Q9IkA0IPLBWpbupwr";
+
+        MqttFactory factory;
+        IMqttClient mqttClient;
+        MqttClientOptionsBuilder options;
+
+        static List<string> connectedClientSessionIds;
+        string uuidString;
+        public MQTTConnection()
+        {
+            Guid uuid = Guid.NewGuid();
+            uuidString = uuid.ToString();
+            connectedClientSessionIds = new List<string>();
+
+            factory = new MqttFactory();
+            mqttClient = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
+                .WithTcpServer(brokerAddress, brokerPort)
+                .WithCredentials(username, password)
+                .WithTls(
+                o =>
+                {
+                    o.CertificateValidationHandler = _ => true;
+
+                    o.SslProtocol = SslProtocols.Tls12;
+
+                })
+                .WithCleanSession()
+                .Build();
+        }
+
+        public static async Task Publish(IMqttClient mqttClient, string message, string topic)
+        {
+            try
+            {
+                var applicationMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(message)
+                    //.WithRetainFlag()     // Retain Flag belässt Nachricht im Topic
+                    .Build();
+
+                MqttClientPublishResult publishResult = await mqttClient.PublishAsync(applicationMessage);
+
+                if (publishResult.ReasonCode == MqttClientPublishReasonCode.Success)
+                {
+                    //Console.WriteLine($"Deine Nachricht: {message}");
+                }
+                else
+                {
+                    Console.WriteLine($"Fehler beim Publishen der Nachricht: {publishResult.ReasonString}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+            }
+        }
+
+        public static async Task Subscribe(IMqttClient mqttClient, string topic, string uuid)
+        {
+            try
+            {
+                await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(topic).Build());
+
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    string messageJSON = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+
+                    var messageData = JsonConvert.DeserializeObject<MessageData>(messageJSON);
+
+                    if (messageData.senderId != uuid)
+                    {
+                        Console.WriteLine($">>Antwort: Topic = {e.ApplicationMessage.Topic}, messageContent = {messageData.content}, messageType = {messageData.messageType}");
+                    }
+                    return Task.CompletedTask;
+                };
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+            }
+        }
+
+        public static async Task Connect(IMqttClient mqttClient, MqttClientOptions options)
+        {
+            try
+            {
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+                Console.WriteLine("Verbunden mit MQTT Broker!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+            }
+        }
+
+        public static async Task Disconnect(IMqttClient mqttClient)
+        {
+            try
+            {
+                await mqttClient.DisconnectAsync();
+                Console.WriteLine("Verbindung getrennt!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+            }
+        }
+    }
+   
+}
