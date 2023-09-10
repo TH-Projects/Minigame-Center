ca865e39e6c69752f84511070bdf1dd7b382161c
Maximilian Müller <maximilian.mueller@mail.ch>
2023-09-10 10:51:12 +0200

    Hinzufügen von Kommentaren im Model v. 4 Gewinnt bzw. Core Datei

--- minigame-center/minigame-center/ViewModel/VierGewinntViewModel.cs
+++ minigame-center/minigame-center/ViewModel/VierGewinntViewModel.cs
@@ -17,14 +17,16 @@ namespace minigame_center.ViewModel
 {
     public class VierGewinntViewModel : BaseViewModel
     {
-        public  string PlayerLabel { get; set; }
-        public  string MoveLabel { get; set; }
+        // Diese Klasse implementiert die Logik für ein Vier-gewinnt-Spiel in einer WPF-Anwendung.
 
-        private static bool  waitBool = false;
+        // Deklaration von Eigenschaften und Feldern
+        public string PlayerLabel { get; set; } // Anzeige des aktuellen Spielers
+        public string MoveLabel { get; set; }   // Anzeige des aktuellen Spielzugs
 
-        
-        public static Ellipse[,] circlesArray;
-        private Grid gameGrid;
+        private static bool waitBool = false; // Eine Flagge, die verwendet wird, um auf Benutzeraktionen zu warten
+
+        public static Ellipse[,] circlesArray; // Ein Array von Ellipsen, die das Spielbrett repräsentieren
+        private Grid gameGrid;                 // Das Hauptspiel-Gitter
 
         private Grid _viewGeneratedGrid;
         public Grid ViewGeneratedGrid
@@ -37,29 +39,32 @@ namespace minigame_center.ViewModel
             }
         }
 
-        private static GameResult gameResult = GameResult.Running;
+        private static GameResult gameResult = GameResult.Running; // Der aktuelle Spielstatus
 
-        public static MQTTGameClient mq;
+        public static MQTTGameClient mq; // Eine MQTT-Verbindung zur Kommunikation mit dem Spielserver
 
+        // Ein Delegat, der verwendet wird, um auf eingehende Spiel-Payloads zu reagieren
         private void PayloadHandler(BasePayload payload)
         {
+            // Aktualisiere die GUI entsprechend den empfangenen Spielinformationen
             UpdateGUI();
 
             if (MQTTGameClient.oponnent == Guid.Empty && payload.sender != MQTTGameClient.clientID)
             {
+                // Wenn der Gegner noch nicht festgelegt ist, setze ihn basierend auf dem Absender der Payload
                 MQTTGameClient.oponnent = payload.sender;
             }
+
             if (MQTTGameClient.player_number == 1)
             {
                 PlayerLabel = "Spieler 1 (Rot)";
-
             }
             else
             {
                 PlayerLabel = "Spieler 2 (Grün)";
-
             }
             OnPropertyChanged(nameof(PlayerLabel));
+
             if (payload.sender != MQTTGameClient.clientID)
             {
                 MoveLabel = "Du bist am Zug!";
@@ -71,57 +76,64 @@ namespace minigame_center.ViewModel
             OnPropertyChanged(nameof(MoveLabel));
         }
 
+        // Konstruktor der Klasse
         public VierGewinntViewModel()
-        {              
+        {
+            // Initialisierung der MQTT-Verbindung und des Spielfelds
             mq = new MQTTGameClient("4gewinnt", PayloadHandler);
             Connect_Four.initialiazeField();
             Connect_Four.CurrentPlayer = MQTTGameClient.player_number;
             Connect_Four.Field_X = 7;
             Connect_Four.Field_Y = 5;
-            
-            PlayerLabel = "Suche Spieler...";
+
+            PlayerLabel = "Suche Spieler..."; // Anfangsanzeige
             MoveLabel = "";
             OnNavigatedTo();
 
+            // Event-Handler für das Beenden der Anwendung
             AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
-
         }
 
-        static void OnProcessExit(object sender, EventArgs e)  // This function is called automatically when the application is terminated.
+        // Handler für das Beenden der Anwendung
+        static void OnProcessExit(object sender, EventArgs e)
         {
-            if (!(MQTTGameClient.currentMessage.gamestatus == GameStatus.RUNNING && MQTTGameClient.player_number == 0) // if a player enters the game during a runing partie
-                && MQTTGameClient.game_state != GameStatus.FINISHED)  
+            // Dieser Code wird automatisch ausgeführt, wenn die Anwendung beendet wird.
+
+            // Überprüfe, ob das Spiel noch läuft und ein Spieler während des Spiels beigetreten ist
+            if (!(MQTTGameClient.currentMessage.gamestatus == GameStatus.RUNNING && MQTTGameClient.player_number == 0)
+                && MQTTGameClient.game_state != GameStatus.FINISHED)
             {
-                // Wenn der oponnent gesetzt wurde wird beim schließen der Applikation eine Nachricht abgesendet in der der
-                // der Gegner als gewinner definiert wird
+                // Wenn der Gegner festgelegt wurde und das Spiel noch nicht vorbei ist,
+                // sende eine Nachricht, in der der Gegner als Gewinner definiert wird.
                 if (MQTTGameClient.oponnent != Guid.Empty && MQTTGameClient.currentMessage.gamestatus == GameStatus.RUNNING)
                 {
                     BasePayload payload = new BasePayload();
                     payload.buildGameFinishedMsg(MQTTGameClient.clientID, MQTTGameClient.oponnent, Connect_Four.getGameFieldAsArray());
                     mq.SendPayload(payload);
-                    
                 }
                 else
                 {
+                    // Andernfalls, wenn der Gegner nicht festgelegt ist, veröffentliche einfach eine leere Nachricht.
                     mq.Publish(null, "4gewinnt");
-
                 }
             }
         }
 
-
+        // Methode zum Einrichten der MQTT-Verbindung
         public static async void Setup()
         {
             await mq.Setup();
-            Thread.Sleep(400);
-            waitBool = true;
+            Thread.Sleep(400); // Warte kurz auf die Einrichtung
+            waitBool = true;   // Setze die Warteflagge auf "true", um Benutzeraktionen zuzulassen
         }
 
+        // Methode, die aufgerufen wird, wenn die Ansicht navigiert wird
         public void OnNavigatedTo()
         {
-            GenerateGrid(6, 7);
+            GenerateGrid(6, 7); // Generiere das Spielbrett
         }
 
+        // Methode zum Generieren des Spielbretts (Rasters)
         public void GenerateGrid(int rows, int columns)
         {
             gameGrid = new Grid();
@@ -138,12 +150,13 @@ namespace minigame_center.ViewModel
 
             ViewGeneratedGrid = gameGrid;
 
+            // Erstelle Schaltflächen (Buttons) für das Einwerfen von Spielsteinen
             for (int col = 0; col < columns; col++)
             {
                 Button dropButton = new Button();
                 dropButton.Content = "*";
 
-                // Setze den Style des Buttons
+                // Setze den Stil des Buttons (aus den Anwendungsressourcen)
                 dropButton.Style = (Style)Application.Current.Resources["DarkButtonStyle"];
 
                 dropButton.VerticalAlignment = VerticalAlignment.Stretch;
@@ -155,11 +168,11 @@ namespace minigame_center.ViewModel
                 gameGrid.Children.Add(dropButton);
                 Grid.SetRow(dropButton, 0);
                 Grid.SetColumn(dropButton, col);
-
             }
 
             circlesArray = new Ellipse[rows, columns];
 
+            // Erstelle Ellipsen für das Spielfeld
             for (int row = 1; row < rows; row++)
             {
                 for (int col = 0; col < columns; col++)
@@ -168,7 +181,7 @@ namespace minigame_center.ViewModel
                     blackCircle.Fill = Brushes.DarkBlue;
                     blackCircle.VerticalAlignment = VerticalAlignment.Stretch;
                     blackCircle.HorizontalAlignment = HorizontalAlignment.Stretch;
-                    blackCircle.Margin = new Thickness(17,15,17,15);
+                    blackCircle.Margin = new Thickness(17, 15, 17, 15);
                     gameGrid.Children.Add(blackCircle);
                     Grid.SetRow(blackCircle, row);
                     Grid.SetColumn(blackCircle, col);
@@ -178,12 +191,12 @@ namespace minigame_center.ViewModel
             }
         }
 
-
+        // Methode zum Aktualisieren der GUI basierend auf dem aktuellen Spielstatus
         public static void UpdateGUI()
         {
-
             Application.Current.Dispatcher.Invoke(() =>
-            {              
+            {
+                // Führe je nach Spielstatus entsprechende Aktionen aus
                 switch (gameResult)
                 {
                     case GameResult.Won:
@@ -195,17 +208,19 @@ namespace minigame_center.ViewModel
                     case GameResult.Running:
                         if (MQTTGameClient.currentMessage.gamefield != null)
                         {
-                            if(
-                                (MQTTGameClient.currentMessage.winner != MQTTGameClient.clientID) && 
+                            if (
+                                (MQTTGameClient.currentMessage.winner != MQTTGameClient.clientID) &&
                                 (MQTTGameClient.currentMessage.winner != Guid.Empty)
-                            ){
+                            )
+                            {
                                 MQTTGameClient.game_state = GameStatus.FINISHED;
                                 App.MainViewModel.NavigateToPage(new LoseMessageViewModel(), "Ende des Spiels");
                             }
-                            else if(
-                                (MQTTGameClient.currentMessage.gamestatus == GameStatus.FINISHED) && 
+                            else if (
+                                (MQTTGameClient.currentMessage.gamestatus == GameStatus.FINISHED) &&
                                 (MQTTGameClient.currentMessage.winner == Guid.Empty)
-                            ){
+                            )
+                            {
                                 MQTTGameClient.game_state = GameStatus.FINISHED;
                                 App.MainViewModel.NavigateToPage(new DrawMessageViewModel(), "Ende des Spiels");
                             }
@@ -217,7 +232,9 @@ namespace minigame_center.ViewModel
                                 MQTTGameClient.game_state = GameStatus.FINISHED;
                                 App.MainViewModel.NavigateToPage(new WinMessageViewModel(), "Der Gegner hat aufgegeben");
                             }
-                            else { 
+                            else
+                            {
+                                // Aktualisiere das Spielfeld basierend auf den empfangenen Spielinformationen
                                 Connect_Four.setGamefieldFromArray(MQTTGameClient.currentMessage.gamefield);
 
                                 int[][] GameField = Connect_Four.getGameFieldAsArray();
@@ -227,26 +244,23 @@ namespace minigame_center.ViewModel
                                     {
                                         if (GameField[i][j] == 0) circlesArray[i + 1, j].Fill = Brushes.DarkBlue;
                                         else if (GameField[i][j] == 1) circlesArray[i + 1, j].Fill = Brushes.Red;
-                                        else if (GameField[i][j] == 2) circlesArray[i + 1, j].Fill = Brushes.Green; // Änderung von 1 auf 2
+                                        else if (GameField[i][j] == 2) circlesArray[i + 1, j].Fill = Brushes.Green;
                                     }
                                 }
                             }
                         }
                         break;
                 }
-                
-                
             });
         }
 
-
-
+        // Handler für das Klicken auf einen Drop-Button
         private void DropButton_Click(object circle)
         {
             if (waitBool == true)
             {
                 int column = (int)circle;
-                //This if statement checks if the game is still in RUNNING state and if the current message isn't from yourself
+                // Diese if-Anweisung überprüft, ob das Spiel noch läuft und die aktuelle Nachricht nicht von Ihnen stammt
                 if (MQTTGameClient.game_state == GameStatus.RUNNING && MQTTGameClient.currentMessage.sender != MQTTGameClient.clientID)
                 {
                     Connect_Four.CurrentPlayer = MQTTGameClient.player_number;
@@ -255,7 +269,7 @@ namespace minigame_center.ViewModel
                     if (Connect_Four.SetStonePossible(column))
                     {
                         gameResult = Connect_Four.SetStone(column);
-                        //publish a new message
+                        // Sende eine neue Nachricht, die den aktuellen Spielstatus enthält
                         BasePayload newMessage = MQTTGameClient.currentMessage;
                         switch (gameResult)
                         {
@@ -276,7 +290,5 @@ namespace minigame_center.ViewModel
                 }
             }
         }
-
     }
 }
-
